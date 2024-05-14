4cf65276a35bdd8a6686612b9ca0e31ac84a9825
EnesArslan8 <enes8arslan@gmail.com>
2024-05-14 17:02:03 +0300

    resetting default project folders and css

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,5 @@
-import { Inter } from "next/font/google";
-
-
-const inter = Inter({ subsets: ["latin"] });
+import '../styles/reset.css'
+import '../styles/global.css'
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="tr">
-      <body className={inter.className}>{children}</body>
+      <body >{children}</body>
     </html>
   );
 }
