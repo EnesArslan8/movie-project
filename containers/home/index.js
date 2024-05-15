702b5447f51f442590ecbef2f47a25dfb7fb1169
EnesArslan8 <enes8arslan@gmail.com>
2024-05-15 11:50:55 +0300

    added movies section

--- containers/home/index.js
+++ containers/home/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Movies from '../../mocks/movies.json'
 import Genres from '../../mocks/genres.json'
-import {Categories, FeaturedMovie} from '../../components'
+import {Categories, FeaturedMovie,MoviesSection} from '../../components'
 const HomeContainer = () => { 
   return (
     <div>
       <FeaturedMovie movie={Movies.results[0]}/>
       <Categories categories={Genres.genres}/>
+      <MoviesSection title='Popular Films' movies={Movies.results.slice(1,7)}/>
+      <MoviesSection title='Your Favorites' movies={Movies.results.slice(7,13)}/>
     </div>
   )
 }
